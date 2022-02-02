702f286e25a58831a707585c0409c33a0f47b65c
Philippe Ngom <philngom@gmail.com>
2022-02-01 20:36:23 -0800

    completed OrderNameInput functionality. Added all images for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import OrderNameInput from './OrderNameInput/OrderNameInput';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [sideId, setFideId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [instructions, setInstructionsId] = useState('');
+  const [orderName, setOrderName] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OrderNameInput setOrderName={ setOrderName } orderName={orderName}/>
     </div>
   );
 }
